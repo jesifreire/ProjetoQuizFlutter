43714dfda0714be298f2a414e2be0b4ae74fabfa
Jesiane <jesianef@hotmail.com>
2021-03-25 21:51:40 -0300

    Primeira parte do desenvolvimento

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -7,13 +7,20 @@ void main() {
 class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Perguntas = [
+      'Qual a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Pergunta App'),
+          title: Text('Perguntas'),
         ),
         body: Column(
-          children: <Widget>[],
+          children: <Widget>[
+            Text(Perguntas[0]),
+          ],
         ),
       ),
     );
