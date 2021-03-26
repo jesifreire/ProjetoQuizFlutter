fdfefa7f9908b923efa2692abb50ad50c5f0e39b
Jesiane <jesianef@hotmail.com>
2021-03-25 21:21:02 -0300

    ProjetoFlutter Quiz

---
+++ projeto_perguntas/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Pergunta App'),
+        ),
+        body: Column(
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
