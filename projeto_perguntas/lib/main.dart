deb562f33797a601eee263596e67d2838a6cd8e4
Jesiane <jesianef@hotmail.com>
2021-04-02 16:26:55 -0300

    Evoluindo projeto, utilizando Map

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:projeto_perguntas/questao.dart';
+import 'resultado.dart';
+import 'questionario.dart';
 
-import 'resposta.dart';
-
-void main() {
+main() {
   runApp(PerguntaApp());
 }
 
@@ -14,36 +13,79 @@ class PerguntaApp extends StatefulWidget {
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
+  var _pontuacaoTotal = 0;
+  final _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': [
+        {'texto': 'Preto', 'pontuacao': 10},
+        {'texto': 'Azul', 'pontuacao': 10},
+        {'texto': 'Verde', 'pontuacao': 10},
+        {'texto': 'Branco', 'pontuacao': 10},
+      ],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': [
+        {'texto': 'Cachorro', 'pontuacao': 10},
+        {'texto': 'Gato', 'pontuacao': 8},
+        {'texto': 'Baleia', 'pontuacao': 10},
+        {'texto': 'Peixe', 'pontuacao': 9},
+      ],
+    },
+    {
+      'texto': 'Qual o seu desenho favorito?',
+      'respostas': [
+        {'texto': 'Snoopy', 'nota': 10},
+        {'texto': 'Três Espiãs Demais', 'nota': 9},
+        {'texto': 'Dragon Ball', 'nota': 8},
+        {'texto': 'She-Ra', 'nota': 10},
+      ],
+    }
+  ];
 
-  void _responder() {
-    setState(() {
-      //Vai aplicar a mudança no estado quando clicar no botão 1
-      _perguntaSelecionada++;
-    });
+  void _responder(int pontuacao) {
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+        _pontuacaoTotal += pontuacao;
+      });
+    }
+    print(_pontuacaoTotal);
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      'Qual a sua cor favorita?',
-      'Qual é o seu animal favorito?',
-    ];
+    List<Map<String, Object>> respostas = temPerguntaSelecionada
+        ? _perguntas[_perguntaSelecionada]['respostas']
+        : null;
 
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
-          title: Text('Perguntas'),
-          backgroundColor: Colors.purple[400],
+          title: Text(
+            'Perguntas',
+            style: TextStyle(
+              color: Colors.white,
+              fontStyle: FontStyle.italic,
+              fontSize: 25,
+            ),
+          ),
+          backgroundColor: Colors.purple[300],
           centerTitle: true,
         ),
-        body: Column(
-          children: <Widget>[
-            Questao(perguntas[_perguntaSelecionada]),
-            Resposta('Resposta 1', _responder),
-            Resposta('Resposta 2', _responder),
-            Resposta('Resposta 3', _responder),
-          ],
-        ),
+        body: temPerguntaSelecionada
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                responder: _responder,
+              )
+            : Resultado(),
       ),
     );
   }
--- projeto_perguntas/lib/resposta.dart
+++ projeto_perguntas/lib/resposta.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
-  final String resposta;
+  final String texto;
   final void Function() onClick;
 
-  Resposta(this.resposta, this.onClick);
+  Resposta(this.texto, this.onClick);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: RaisedButton(
-        child: Text(resposta),
+        child: Text(texto),
         textColor: Colors.white,
-        color: Colors.purple,
+        color: Colors.purple[300],
         onPressed: onClick,
       ),
     );
